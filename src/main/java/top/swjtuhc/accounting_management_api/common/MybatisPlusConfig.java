6dd4059b1a1377cec49c7ec18fb30b43e1d9e9cf
骆俊杰 <1705129148@qq.com>
2025-12-10 20:23:42 +0800

    feat（新增mybatisPlusConfig配置类，解决了total为0的问题）

---
+++ src/main/java/top/swjtuhc/accounting_management_api/common/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package top.swjtuhc.accounting_management_api.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 新增分页拦截器，并设置数据库类型为mysql
+     * 解决了能查到record数据但是total为0的问题
+     */
+    @Bean(name = "mybatisPlusInterceptor")
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
